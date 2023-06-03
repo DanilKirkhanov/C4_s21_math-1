d98425ccd11fcf1c6de22b9b8ebf3bd102f72a43
Strange Milan <strangem@student.21-school.ru>
2023-06-03 19:57:39 +0300

    log, pow, exp, sqrt, fabs, abs

---
+++ src/s21_math.c
@@ -0,0 +1,113 @@
+#include "s21_math.h"
+#include "stdio.h"
+
+int abs(int x) {
+    if(x < 0) x *= -1;
+    return x;
+}
+
+// long double acos(double x) {
+
+// }
+
+// long double asin(double x) {
+
+// }
+//вычисляет арксинус
+
+// long double atan(double x) {
+
+// }
+//вычисляет арктангенс
+
+long double ceil(double x) {
+    return (int)x + 1;
+}
+//возвращает ближайшее целое число, не меньшее заданного значения
+
+// long double cos(double x){
+//     for 
+// }
+
+long double exp(double x){
+    long double sum = 1;
+    long double term = 1;
+    for (int i = 1; i < 10000; i++) {
+        term = term * x / i;
+        sum += term;
+    }
+    return sum;
+}
+//возвращает значение e, возведенное в заданную степень
+
+long double fabs(double x){
+    long unsigned mask = 0x7FFFFFFFFFFFFFFF; // Маска для обнуления знакового бита
+    long unsigned bits = *(long unsigned *)&x; // Получаем битовое представление x
+    bits &= mask; // Обнуляем знаковый бит
+    return *(double *)&bits;
+}
+//вычисляет абсолютное значение числа с плавающей точкой
+
+long double floor(long double x){
+    long double com = x;
+    printf("%Lf\n", x);
+    if(com < (int)x) x--;
+    return (int)x;
+}
+//возвращает ближайшее целое число, не превышающее заданное значение
+
+// long double fmod(double x, double y){
+
+// }
+// //остаток операции деления с плавающей точкой
+
+long double log(double x){
+    double y = x;
+    for (int i = 0; i < 1000; i++) {
+        y = y - (exp(y) - x) / exp(y);
+    }
+    return y;
+}
+
+long double pow(double base, double expo){
+    // int mult = base;
+    // if(exp < 0) {
+    //     base = 1/base;
+    //     exp *= -1;
+    // }
+    // if(exp > 0){
+    //     for (int i = 1; i < exp; exp--) {
+    //         base *= mult;
+    //     }
+    //     if(exp > 0 && exp != 1) base = pow(base, (1/exp));
+    // }
+    // return base;
+    return exp(expo * log(base));
+}
+
+// long double sin(double x){
+
+// }
+
+long double sqrt(double x){
+//   double x = 1; // Начальное приближение
+//   int a = 0;
+//   double eps = 0.000001; // Точность
+//   double dx; // Разность между двумя приближениями
+//   do {
+//     dx = (x * x - a) / (2 * x); // Вычисляем разность
+//     x = x - dx; // Вычисляем следующее приближение
+//   } while (fabs(dx) > eps); // Повторяем пока разность больше точности
+//   return x; // Возвращаем приближенный корень
+    long double ans = pow(x, 0.5);
+    // printf("%f\n", pow(x, 2));
+    return ans;
+}
+
+// long double tan(double x){
+
+// }
+int main(){
+    printf("%Lf\n", floor(2.9999999999999999));
+    return 0;
+}
---
+++ src/s21_math.h
@@ -0,0 +1,43 @@
+#ifdef S21_MATH_H
+#define S21_MATH_H
+
+int abs(int x);
+//вычисляет абсолютное значение целого числа
+
+// long double acos(double x);
+//вычисляет арккосинус
+
+// long double asin(double x);
+//вычисляет арксинус
+
+// long double atan(double x);
+//вычисляет арктангенс
+
+long double ceil(double x);
+//возвращает ближайшее целое число, не меньшее заданного значения
+
+// long double cos(double x);
+
+long double exp(double x);
+//возвращает значение e, возведенное в заданную степень
+
+long double fabs(double x);
+//вычисляет абсолютное значение числа с плавающей точкой
+
+long double floor(double x);
+//возвращает ближайшее целое число, не превышающее заданное значение
+
+// long double fmod(double x, double y);
+//остаток операции деления с плавающей точкой
+
+long double log(double x);
+
+long double pow(double base, double exp);
+
+// long double sin(double x);
+
+long double sqrt(double x);
+
+// long double tan(double x);
+
+#endif 
