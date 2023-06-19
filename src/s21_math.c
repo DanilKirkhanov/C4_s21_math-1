bbe3ea9f19e86a7318b1b93922a6ad5d66504117
Arson Dorotha <arsondor@un-e5.kzn.21-school.ru>
2023-06-19 23:58:43 +0300

    Changed pow

--- src/s21_math.c
+++ src/s21_math.c
@@ -1,13 +1,17 @@
 #include "s21_math.h"
 
-// #include <math.h>
+#include <math.h>
 #include <stdio.h>
 
 #define LN2 0.693147180559945309417232
+#define S21_INF 1.0 / 0.0
+#define S21_NAN 0.0 / 0.0
 long double s21_fuctorial(long long int num);
 long double s21_pow_int(double base, double expo);
 
-int s21_abs(int x) { return x < 0 ? x : -x; }
+int s21_abs(int x) {
+  if (x) return x < 0 ? x : -x;
+}
 
 // long double acos(double x) {
 
@@ -24,7 +28,10 @@ int s21_abs(int x) { return x < 0 ? x : -x; }
 // вычисляет арктангенс
 
 long double s21_ceil(double x) {
-  int xi = (int)x;
+  long double xi = (long int)x;
+  if (x != x || x != S21_INF && x != -S21_INF) {
+    xi = x;
+  }
   return x < xi ? xi + 1 : xi;
 }
 // возвращает ближайшее целое число, не меньшее заданного значения
@@ -34,25 +41,25 @@ long double s21_ceil(double x) {
 // }
 
 long double s21_exp(double x) {
-  //   long double sum = 1;
-  //   long double term = 1;
-  //   for (int i = 1; i < 1000; i++) {
-  //     term = term * x / i;
-  //     sum += term;
-  //   }
-  //   long double sum = 2.71828182845904523536;
-  if (x == -1.0 / 0) return 0;
   long double result = 0;
-  long double a = (long long int)(x / LN2);
-  long double b = x - a * LN2;
-  //   printf("a = %Lf, b = %Lf\n", a, b);
-  for (int i = 0; i < 50; i++) {
-    result += s21_pow_int(b, i) / s21_fuctorial(i);
-    // printf("%Lf\n", s21_pow_int(b, i));
+  long double a = x > 0 ? (long long int)(x / LN2) : (long long int)(-x * LN2);
+  long double b = x > 0 ? x - a * LN2 : x + a * LN2;
+  if (x > 710)
+    result = S21_INF;
+  else if (x <= -15)
+    result = 0.0;
+  else if (!x)
+    result = 1;
+  else {
+    for (int i = 0; i < 50; i++) {
+      result += s21_pow_int(b, i) / s21_fuctorial(i);
+    }
+    if (x > 0)
+      result *= s21_pow_int(2, a);
+    else
+      result /= s21_pow_int(2, a);
   }
-  result *= s21_pow_int(2, a);
   return result;
-  //   return sum;
 }
 // возвращает значение e, возведенное в заданную степень
 
@@ -62,23 +69,25 @@ long double s21_pow_int(double base, double expo) {
     base = 1 / base;
     expo *= -1;
   }
-  //   printf("%Lf\n", mult);
   if (expo == 0) base = 1;
   if (expo > 0) {
     for (int i = 1; i < expo; expo--) {
       base *= mult;
     }
-    // if (expo > 0 && expo != 1) base = s21_pow(base, (1 / expo));
   }
   return base;
 }
 
 long double s21_fuctorial(long long int num) {
   long double res = 1.0;
-  if (num == 1 || num == 0) return 1;
-  if (num < 0) return -1.0 / 0;
-  for (int i = 2; i <= num; i++) {
-    res *= i;
+  if (num == 1 || num == 0)
+    res = 1;
+  else if (num < 0)
+    res = -1;  // ERROR //return -1.0 / 0;
+  else {
+    for (int i = 2; i <= num; i++) {
+      res *= i;
+    }
   }
   return res;
 }
@@ -99,9 +108,11 @@ long double s21_floor(long double x) {
 }
 // возвращает ближайшее целое число, не превышающее заданное значение
 
-// long double fmod(double x, double y){
-
-// }
+long double s21_fmod(double x, double y) {
+  long double num = 0;
+  num = x - (int)(x / y) * y;
+  return num;
+}
 // //остаток операции деления с плавающей точкой
 
 long double s21_log(double x) {
@@ -112,20 +123,29 @@ long double s21_log(double x) {
   return y;
 }
 
-long double s21_pow(double base, double expo) {
-  // int mult = base;
-  // if(s21_exp < 0) {
-  //     base = 1/base;
-  //     s21_exp *= -1;
-  // }
-  // if(s21_exp > 0){
-  //     for (int i = 1; i < s21_exp; s21_exp--) {
-  //         base *= mult;
-  //     }
-  //     if(s21_exp > 0 && s21_exp != 1) base = s21_pow(base, (1/s21_exp));
-  // }
-  // return base;
-  return s21_exp(expo * s21_log(base));
+long double s21_pow(double base, double exp) {
+  long double result = 1;
+  if (exp == 0 || base == 1 ||
+      ((exp == -S21_INF || exp == S21_INF) && base == -1))
+    ;
+  else if (exp == 1)
+    result = base;
+  else if (base == 0 && exp > 0 ||
+           ((base == -S21_INF || base == S21_INF) && exp < 0) ||
+           (exp == -S21_INF && base != 0))
+    result = 0;
+  else if (base == 0 && exp < 0 ||
+           ((base == -S21_INF || base == S21_INF) && exp > 0) || exp == S21_INF)
+    result = S21_INF;
+  else if (base != base || exp != exp || (base < 0 && exp != (int)exp))
+    result = S21_NAN;
+
+  else {
+    if (base < 0 && s21_fabs(s21_fmod(exp, 2)) == 1) result = -1;
+    result *= s21_exp(exp * s21_log(s21_fabs(base)));
+  }
+
+  return result;
 }
 
 // long double sin(double x){
@@ -133,17 +153,7 @@ long double s21_pow(double base, double expo) {
 // }
 
 long double s21_sqrt(double x) {
-  //   double x = 1; // Начальное приближение
-  //   int a = 0;
-  //   double eps = 0.000001; // Точность
-  //   double dx; // Разность между двумя приближениями
-  //   do {
-  //     dx = (x * x - a) / (2 * x); // Вычисляем разность
-  //     x = x - dx; // Вычисляем следующее приближение
-  //   } while (s21_fabs(dx) > eps); // Повторяем пока разность больше точности
-  //   return x; // Возвращаем приближенный корень
   long double ans = s21_pow(x, 0.5);
-  // printf("%f\n", s21_pow(x, 2));
   return ans;
 }
 
@@ -151,7 +161,21 @@ long double s21_sqrt(double x) {
 
 // }
 int main() {
-  printf("%Lf\n", s21_sqrt(16));
-  //   printf("%Lf\n", log(10000.0));
+  double a[6] = {NAN, 0, 1, -1, INFINITY, -INFINITY};
+  int count = 1;
+  for (int i = 0; i < 6; i++) {
+    for (int j = 0; j < 6; j++) {
+      printf("%d)base %.0lf exp %.0lf\n", count, a[i], a[j]);
+      printf("pow :%.0lf\n s21_pow:%.0Lf\n\n", pow(a[i], a[j]),
+             s21_pow(a[i], a[j]));
+      count++;
+    }
+  }
+  // int i = 0;
+  // while (1) {
+  // printf("original = %lf\n", pow(INFINITY, 0));
+  // printf("Our Krym = %Lf\n", s21_pow(NAN, NAN));
+  // i++;
+  // }
   return 0;
 }
